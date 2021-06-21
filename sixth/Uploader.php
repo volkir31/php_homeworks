92642733eb6b6c095570c43c637e7c8c90b4ecb0
volkir31 <egor_mixajlichenko@mail.ru>
2021-06-21 12:39:53 +0300

    done sixth homework second part

--- sixth/Uploader.php
+++ sixth/Uploader.php
@@ -1,1 +1,44 @@
 <?php
+
+class Uploader
+{
+
+    protected string $field;
+    protected bool $uploaded = false;
+
+    public function __construct(string $fieldName)
+    {
+        $this->field = $fieldName;
+    }
+
+    /**
+     * checks if the file is uploaded
+     * @return object
+     */
+    public function isUploaded(): object
+    {
+        if(0 === $_FILES[$this->field]['error']) {
+            $this->uploaded = true;
+        }
+        return $this;
+//        return 0 === $_FILES[$this->field]['error'];
+    }
+
+    /**
+     * save uploaded file to directory
+     * @return bool
+     */
+    public function upload(): bool
+    {
+        if ($this->uploaded) {
+            $name = $_FILES[$this->field]['name'];
+            move_uploaded_file($_FILES[$this->field]['tmp_name'], __DIR__ . "/files/$name");
+            return true;
+        }
+        return false;
+    }
+}
+
+$a = new Uploader('file');
+$a->isUploaded()->upload();
+header('location: /php_homeworks/sixth/index2.php');
