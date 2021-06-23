cca8b98d1d8d200aa68aba2a4140fe9fa3c3d699
volkir31 <egor_mixajlichenko@mial.ru>
2021-06-23 23:27:02 +0300

    done eighth homework

--- eighth/index.php
+++ eighth/index.php
@@ -2,16 +2,19 @@
 require_once __DIR__ . '/View.php';
 require_once __DIR__ . '/DB.php';
 echo '<link rel="stylesheet" href="templates/style.css">';
-$view = new View();
 
+$view = new View();
 $db = new DB(require __DIR__ . '/config.php');
+
+if ($_POST['author'] && $_POST['title'] && $_POST['text']) {
+//    $post = ['author' => $_POST['author'], 'title' => $_POST['title'], 'text' => $_POST['text']];
+    $post = [$_POST['title'], $_POST['text'], $_POST['author']];
+    $db->query("INSERT INTO users.news (title, text, author) VALUES (?, ?, ?)", $post);
+    $_POST = [];
+}
 
 $data = $db->query("SELECT * FROM users.news ORDER BY id DESC", []);
 
-if ($_POST['author'] && $_POST['title'] && $_POST['text']) {
-    $post = ['author' => $_POST['author'], 'title' => $_POST['title'], 'text' => $_POST['text']];
-    $db->query("INSERT INTO users.news (title, text, author) VALUES (?, ?, ?)", $post);
-}
 foreach ($data as $index => $datum) {
     $view->assign($index, $datum);
 }
